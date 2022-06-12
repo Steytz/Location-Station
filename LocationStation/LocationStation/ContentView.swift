319b1cb13ca3e631f3c070640490822d9ef39c76
Steytz <lucianofreelancingde@gmail.com>
2022-06-12 17:52:36 +0200

    Add project initial xcode setup
    The setup and initial project was added

---
+++ LocationStation/LocationStation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationStation
+//
+//  Created by Steyt on 12.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
