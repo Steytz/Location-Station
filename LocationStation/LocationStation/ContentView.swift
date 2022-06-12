bdc5d5d0920f50fcab98f772689ab1a69e176a92
Steytz <lucianofreelancingde@gmail.com>
2022-06-12 18:46:38 +0200

    Add user Location & Permissions
    The user Location was added delegating the logic
    to a separate viewModel, in which we ask/handle permissions
    in multiple cases and use a integrated delegate which runs
    everytime a locationManager is created/instantiated to tell when
    the permissions have changed. Also updated the Plist to allow
    location while in app use.

--- LocationStation/LocationStation/ContentView.swift
+++ LocationStation/LocationStation/ContentView.swift
@@ -8,14 +8,55 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = ContentViewModel()
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50.92897235052956, longitude: 6.943178858308985), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
     var body: some View {
-        Map(coordinateRegion: $region).ignoresSafeArea()
+        Map(coordinateRegion: $region, showsUserLocation: true)
+            .ignoresSafeArea()
+            .onAppear{
+                viewModel.checkIsLocationServiceOn()
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager?
+    
+    func checkIsLocationServiceOn(){
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+        } else {
+            // Todo show alert to tell user to turn location on
+        }
+    }
+    
+   private func handleLocationPermission() {
+        guard let locationManager = locationManager else { return }
+        switch locationManager.authorizationStatus {
+        
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+           print("Your location is restricted, maybe check for parental controls.")
+        case .denied:
+            print("It seems as if you have denied location permission for this app, please go to settings and give location permissions to this app")
+        case .authorizedAlways, .authorizedWhenInUse:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        handleLocationPermission()
     }
 }
