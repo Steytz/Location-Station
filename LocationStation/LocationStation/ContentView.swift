19e8bebaefd480c08fec948829409cdc2fb07e0b
Steytz <lucianofreelancingde@gmail.com>
2022-06-12 18:05:36 +0200

    Integrate Map with mapkit
    The Map was integrated using Mapkit

--- LocationStation/LocationStation/ContentView.swift
+++ LocationStation/LocationStation/ContentView.swift
@@ -4,13 +4,13 @@
 //
 //  Created by Steyt on 12.06.22.
 //
-
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50.92897235052956, longitude: 6.943178858308985), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region).ignoresSafeArea()
     }
 }
 
