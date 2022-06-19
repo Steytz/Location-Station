4f8bf956ebc456af53c33f49520124dfb7b7c73c
Steytz <lucianofreelancingde@gmail.com>
2022-06-19 21:45:47 +0200

    Add refresh button style and info button
    The refresh button was properly styled also a svg
    was added to the assets for the button. A info button
    layout was also added to the mix

--- LocationStation/LocationStation/ContentView.swift
+++ LocationStation/LocationStation/ContentView.swift
@@ -11,10 +11,16 @@ struct ContentView: View {
                 .onAppear{
                    viewModel.checkIsLocationServiceOn()
                 }
-            
-        }.overlay(Button("Refresh") {
-            viewModel.getApiData()
-        }, alignment: .bottom)
+        }.overlay(alignment: .bottom, content: {
+            HStack(alignment: .center) {
+                PlaceholderView()
+                Spacer()
+                RefreshButton(onPress: viewModel.getApiData)
+                Spacer()
+                InfoButton()
+            }.padding(.horizontal, 14.0)
+                
+        })
        
     }
 }
@@ -24,3 +30,37 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct RefreshButton: View {
+    let onPress: () -> Void
+    var body: some View {
+        Button {
+            onPress()
+        } label: {
+            Image("refresh")
+        }.padding(.all)
+            .frame(width: 63, height: 45)
+            .background(Color(red: 0.41, green: 0.75, blue: 0.94))
+            .cornerRadius(15)
+    }
+}
+
+struct InfoButton: View {
+    var body: some View {
+        Button {
+            print("Info")
+        } label: {
+            Text("i").foregroundColor(Color.black).bold().font(.largeTitle)
+        }.frame(width: 50, height: 50)
+            .background(Color(red: 0.41, green: 0.75, blue: 0.94))
+            .cornerRadius(50)
+    }
+}
+
+struct PlaceholderView: View {
+    var body: some View {
+        Rectangle().frame(width: 50, height: 50)
+            .hidden()
+    }
+}
+
